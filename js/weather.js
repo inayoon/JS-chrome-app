a67490684db37aeb0bcd56834fd3c36adda859d0
Ina Yoon <yia0319@gmail.com>
2022-07-03 12:57:36 -0400

    JS:Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,17 @@
+const API_KEY="74b5416afe859cb679b59f66b9b594e6";
+
 function onGeoOk(position){
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  console.log("You live in ", lat, lng);
-
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response)=>response.json())
+    .then(data=>{
+      const weather= document.querySelector("#weather span:first-child");
+      const city= document.querySelector("#weather span:last-child")
+      city.innerText = data.name;
+      weather.innerText =`${data.weather[0].main} / ${data.main.temp}`;
+  });
 }
 
 function onGeoError(){
