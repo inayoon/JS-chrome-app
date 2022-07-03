a15eef3d6bda67677d98ad0cec95984d9af00e2e
Ina Yoon <yia0319@gmail.com>
2022-07-03 12:10:42 -0400

    JS:Deleting todos

--- js/todo.js
+++ js/todo.js
@@ -12,17 +12,19 @@ function saveToDos(){
 function deleteToDo (event){
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter(toDo=> toDo.id !== parseInt(li.id));
 }
 
 function paintToDo(newTodo){
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   toDoList.appendChild(li);
 }
 
@@ -31,8 +33,12 @@ function handleToDoSubmit (event) {
   console.log(toDoInput.value);
   const newTodo = toDoInput.value;
   toDoInput.value ="";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj ={
+    text:newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
@@ -47,3 +53,4 @@ if(savedToDos){
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
 }
+
