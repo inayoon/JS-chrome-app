f41ce4589148f8f4ab9aec922a866d76a511ccc2
Ina Yoon <yia0319@gmail.com>
2022-07-11 11:08:50 -0400

    Fourth:Fix some at todolist function

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,5 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
@@ -9,32 +9,36 @@ function saveToDos(){
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-function deleteToDo (event){
+
+function deleteToDo(event){
   const li = event.target.parentElement;
   li.remove();
-  toDos = toDos.filter(toDo=> toDo.id !== parseInt(li.id));
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo){
   const li = document.createElement("li");
   li.id = newTodo.id;
   const span = document.createElement("span");
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "❌";
+  button.innerText = "⛔";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo.text;
+
   toDoList.appendChild(li);
+
 }
 
-function handleToDoSubmit (event) {
+
+function handleToDoSubmit(event){
   event.preventDefault();
-  console.log(toDoInput.value);
-  const newTodo = toDoInput.value;
-  toDoInput.value ="";
-  const newTodoObj ={
-    text:newTodo,
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
     id: Date.now(),
   };
   toDos.push(newTodoObj);
@@ -42,13 +46,14 @@ function handleToDoSubmit (event) {
   saveToDos();
 }
 
-toDoForm.addEventListener("submit",handleToDoSubmit);
+toDoForm.addEventListener("submit", handleToDoSubmit);
 
-
+function sayHello (){
+  console.log("hello");
+}
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
-console.log(savedToDos);
-if(savedToDos){
+if(savedToDos!== null){
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
